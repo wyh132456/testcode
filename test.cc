e3dc5311e8942e8de9294c57cc8447e7a577bfbe
wyh132456 <381985997@qq.com>
2020-03-17 13:59:22 +0800

    mast

--- test.cc
+++ test.cc
@@ -1,9 +1,20 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+int __init demo_init(void);
 int main(int argc, char const *argv[])
 {
     cout << "hello" <<endl;
     cout << "world" <<endl;
+    system("pause");
+    return 0;
+}
+
+extern void my_func(void);
+
+int __init demo_init(void)
+{
+    my_func();
+    printK(**********);
     return 0;
 }
